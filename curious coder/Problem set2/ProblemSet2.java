b7afa1d19488a86cf9c4c578e9c73a93c323661c
Srivatsan kumar S <srivatsan1706@gmail.com>
2025-03-28 11:50:48 +0530

    new commit

---
+++ curious coder/Problem set2/ProblemSet2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+import java.util.Scanner;
+public class ProblemSet2
+{
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int i=1;i*x<=1000;i++)
+		{
+		    System.out.println(x*i);
+		}
+		
+	
+		
+	}
+}
---
+++ curious coder/Problem set2/ProblemSet3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import java.util.Scanner;
+public class ProblemSet3
+{
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+        //enna dha nadakatum
+		String FirstName = sc.nextLine();
+		String LastName = sc.nextLine();
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++)
+        {
+            System.out.println(FirstName+LastName);
+        }
+		
+	
+		
+	}
+}
